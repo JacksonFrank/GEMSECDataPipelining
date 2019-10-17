313251dc6f96edb45f37d31790c4ee6545fb76aa
jrfrank <jrfrank@uw.edu>
2019-10-17 14:57:05 -0700

    Properly formatted pdb_cleaning.py, updated .gitignore to ignore cleaned pdb files

--- code/pdb_cleaning.py
+++ code/pdb_cleaning.py
@@ -4,7 +4,7 @@ Created on Thu Jun 27 17:26:55 2019
 
 @author: jtfl2
 
-Helper script to clean pdb folders
+Cleans pdb folders
 """
 
 import cleaning
@@ -13,20 +13,19 @@ import subprocess
 import platform
 import easygui
 
-"""
-This check is for my local machine, as graphical interfaces
-    on Windows Subsystem for Linux is different
-"""
-if 'Microsoft' in platform.release():
-    os.environ["DISPLAY"] = ":0"
-    print "Display: "
-    subprocess.call("echo $DISPLAY", shell=True)
+def cleanPDB():
+    """
+    This check is for my local machine, as graphical interfaces
+        on Windows Subsystem for Linux are different
+    """
+    if 'Microsoft' in platform.release():
+        os.environ["DISPLAY"] = ":0"
+        print "Display: "
+        subprocess.call("echo $DISPLAY", shell=True)
 
+    d = os.getcwd()
 
+    filepath = easygui.fileopenbox('Choose a file', filetypes=["*.pdb"])
 
-d = os.getcwd()
-
-easygui.fileopenbox('Choose a file', '', '')
-
-for i in os.listdir('pdb/'):
-    cleaning.cleanATOM(d + '/pdb/' + i, out_file= d + '/cleaned/' + i, ext = '.pdb')
+    if filepath is not None:
+        cleaning.cleanATOM(filepath)
